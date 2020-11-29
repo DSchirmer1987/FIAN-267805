9985cd1367a5090cf9bbab7f965f6d77e50d6887
Daniel Schirmer <d.schirmer1987@gmail.com>
2020-11-29 22:50:34 +0100

    Initial Commit

---
+++ Tag_04/src/tag_04/Application.java
@@ -0,0 +1,19 @@
+/**
+ * Daniel Schirmer
+ *
+ * 29.11.2020
+ * Project : Tag_04
+ * ©2020
+ *
+ */
+
+package tag_04;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
