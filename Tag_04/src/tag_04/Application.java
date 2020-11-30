101ffad77465ed27ac528efa93f67f2ff920facb
Daniel Schirmer <d.schirmer1987@gmail.com>
2020-11-30 12:47:50 +0100

    Scanner

--- Tag_04/src/tag_04/Application.java
+++ Tag_04/src/tag_04/Application.java
@@ -10,10 +10,16 @@
 package tag_04;
 
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Application {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Bitte eine Zahl eingeben");
+		int eingabe = sc.nextInt();
+		System.out.println("Sie haben: " + eingabe + " eingeben");
+		
 		SumUneven su = new SumUneven(10);
 		su.calculate();
 		
