1bc1a331daf1a18813c44fcb5ee70c0f6e3234d8
Daniel Schirmer <d.schirmer1987@gmail.com>
2020-12-01 09:56:20 +0100

    Tag_05

---
+++ Tag_05/src/Application.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Daniel Schirmer
+ *
+ * 30.11.2020
+ * Project : Tag05
+ * ©2020
+ *
+ */
+
+public class Application {
+
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Bitte eine Zahl eingeben:");
+//		EuroCent ec = new EuroCent(sc.nextInt());
+//		ec.umrechnen();
+//		System.out.println(ec);
+//		sc.close();
+//		
+//		Seconds s = new Seconds();
+//		s.inputSeconds();
+//		s.calculateDaysHoursMinutes();
+//		System.out.println(s);
+		
+	}
+
+}
