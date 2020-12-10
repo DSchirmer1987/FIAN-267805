6db24556eb006994d87124b4e63cf439815f4240
Daniel Schirmer <d.schirmer1987@gmail.com>
2020-12-10 12:05:20 +0100

    GUI Vorlage

---
+++ Tag_12_FahrscheinAutomat/src/fahrscheinautomat/view/MyFrame.java
@@ -0,0 +1,16 @@
+/**
+ * Daniel Schirmer
+ *
+ * 10.12.2020
+ * Project : Tag_12_FahrscheinAutomat
+ * ©2020
+ *
+ */
+
+package fahrscheinautomat.view;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+}
