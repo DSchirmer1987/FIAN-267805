6039b2795af04a8c702960d9c018a21d63089db1
Daniel Schirmer <d.schirmer1987@gmail.com>
2020-12-10 12:34:59 +0100

    GUI Controller

---
+++ Tag_12_FahrscheinAutomat/src/fahrscheinautomat/controller/GUIController.java
@@ -0,0 +1,19 @@
+/**
+ * Daniel Schirmer
+ *
+ * 10.12.2020
+ * Project : Tag_12_FahrscheinAutomat
+ * ©2020
+ *
+ */
+
+package fahrscheinautomat.controller;
+
+import fahrscheinautomat.view.MyFrame;
+
+public class GUIController {
+	public GUIController() {
+		MyFrame frame = new MyFrame("Fahrscheinautomat");
+		frame.setVisible(true);
+	}
+}
