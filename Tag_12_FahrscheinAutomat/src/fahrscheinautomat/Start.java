3b0781ddfeebea62c1bd470e32bca429cefb7d1f
Daniel Schirmer <d.schirmer1987@gmail.com>
2020-12-10 10:18:05 +0100

    Tag_12 Fahrscheinautomat

---
+++ Tag_12_FahrscheinAutomat/src/fahrscheinautomat/Start.java
@@ -0,0 +1,16 @@
+/**
+ * Daniel Schirmer
+ *
+ * 10.12.2020
+ * Project : Tag_12_FahrscheinAutomat
+ * ©2020
+ *
+ */
+
+package fahrscheinautomat;
+
+public class Start {
+
+	public static void main(String[] args) {
+	}
+}
---
+++ Tag_12_FahrscheinAutomat/src/fahrscheinautomat/model/Fahrscheinautomat.java
@@ -0,0 +1,14 @@
+/**
+ * Daniel Schirmer
+ *
+ * 10.12.2020
+ * Project : Tag_12_FahrscheinAutomat
+ * ©2020
+ *
+ */
+
+package fahrscheinautomat.model;
+
+public class Fahrscheinautomat {
+
+}
