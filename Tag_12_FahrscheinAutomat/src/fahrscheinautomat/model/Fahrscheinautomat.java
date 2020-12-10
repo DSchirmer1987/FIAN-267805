9fe7f2a2f6e42b66a5d8a7c9a427fc39956bfcfb
Daniel Schirmer <d.schirmer1987@gmail.com>
2020-12-10 11:14:10 +0100

    Themen

--- Tag_12_FahrscheinAutomat/src/fahrscheinautomat/model/Fahrscheinautomat.java
+++ Tag_12_FahrscheinAutomat/src/fahrscheinautomat/model/Fahrscheinautomat.java
@@ -11,4 +11,5 @@ package fahrscheinautomat.model;
 
 public class Fahrscheinautomat {
 	// Methoden für bezahlen, wechselgeld ausgeben, reset und dergleichen
+	// Ausgabe der Münzen beim Wechselgeld - 2€, 1€, 50Cent, 20cent, 10cent, 5cent, 2cent und 1cent
 }
