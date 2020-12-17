59e4df630348b4206b87eb8850eead2a73be0256
Daniel Schirmer <d.schirmer1987@gmail.com>
2020-12-17 12:53:46 +0100

    DateFormatter

--- Tag_17/src/datetimedemo/FormattedDemo.java
+++ Tag_17/src/datetimedemo/FormattedDemo.java
@@ -16,16 +16,21 @@ public class FormattedDemo {
 
 	public static void main(String[] args) {
 		LocalDateTime datetime1 = LocalDateTime.now();
+		LocalDateTime datetime2 = LocalDateTime.of(2024, 1, 1, 11, 11);
 		System.out.println("Standard: " + datetime1);
 		
 		DateTimeFormatter format1 = DateTimeFormatter.ISO_DATE_TIME;
 		DateTimeFormatter format2 = DateTimeFormatter.ofPattern("dd'.'MM'.'yyyy");
 		DateTimeFormatter format3 = DateTimeFormatter.ofPattern("dd'.'MM'.'yyyy' - 'HH':'m':'s");
+		DateTimeFormatter format4 = DateTimeFormatter.ofPattern("Y'+'w");
+		DateTimeFormatter format5 = DateTimeFormatter.ofPattern("y'+'W");
 
 		
 		System.out.println("ISO_DATE_TIME: " + datetime1.format(format1));
 		System.out.println("Pattern : dd.M.yyyy: " + datetime1.format(format2));
 		System.out.println("Pattern : dd.M.yyyy: " + datetime1.format(format3));
+		System.out.println("Pattern : Y(week-based-year): " + datetime2.format(format4));
+		System.out.println("Pattern : y(year-of-era): " + datetime2.format(format5));
 		
 		
 	}
