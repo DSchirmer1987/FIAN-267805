5a44fdcc39faecb7510ea532a58dd98179dde1f7
Daniel Schirmer <d.schirmer1987@gmail.com>
2020-12-17 09:12:11 +0100

    Test for Katze

---
+++ Tag_17/src/threadTiere/KatzeTest.java
@@ -0,0 +1,28 @@
+/**
+ * Daniel Schirmer
+ *
+ * 17.12.2020
+ * Project : Tag_17
+ * ©2020
+ *
+ */
+
+package threadTiere;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KatzeTest {
+
+	@Test
+	public void testLaufen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKatze() {
+		assertNotNull(new Katze("Tom", 13, true));
+	}
+
+}
