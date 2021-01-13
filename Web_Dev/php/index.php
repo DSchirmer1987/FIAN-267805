2d513c4e05d3a0fe56e5ce45fedd898eb65365e8
Daniel Schirmer <d.schirmer1987@gmail.com>
2021-01-13 12:54:18 +0100

    Aufgaben 13.01.2021

---
+++ Web_Dev/php/index.php
@@ -0,0 +1,15 @@
+<?php
+    $ausgabe = "Hallo PHP-Welt";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <title>My PHP Page</title>
+    </head>
+    <body>
+        <div>
+            <p><?=$ausgabe?></p>
+        </div>
+    </body>
+</html>
--- Web_Dev/readme.md
+++ Web_Dev/readme.md
@@ -3,3 +3,8 @@
 ## 13.01.2021
 
 ### Aufgabe 1:
+Erweitern Sie das AJAX von der XML-Datei so, dass auch die anderen Tags mit ausgegeben werden in der Tabelle.
+Country, Company, Price, Year
+
+### Aufgabe 2:
+Übertragen sie das HTML, CSS und JS in die index.php
