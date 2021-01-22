5c6359b8987a6c512a39b55bbace0ef07eb9f54f
Daniel Schirmer <d.schirmer1987@gmail.com>
2021-01-22 12:57:29 +0100

    Klassendiagramm

--- Arrays/src/App.java
+++ Arrays/src/App.java
@@ -3,7 +3,34 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int zahl = 5;
+		if(zahl == 1 ) {
+			
+		} else if(zahl == 2) {
+			
+		} else if(zahl == 3) {
+			
+		} else {
+			
+		}
 	}
+	
+	public static void essen(String name, int anzahl) {
+		
+	}
+	
+	public static void essen(int anzahl, String name) {
+		
+	}
+	
+//	public static Gericht gerichtZubereiten() {
+//		
+//	}
+//	
+//	public static void gerichtServieren(Gericht gericht) {
+//		
+//	}
+	
+	
 
 }
---
+++ Arrays/src/Mitarbeiter.java
@@ -0,0 +1,5 @@
+
+public class Mitarbeiter {
+	public Mitarbeiter vorgesetzter;
+	public Mitarbeiter[] unterstellter;
+}
