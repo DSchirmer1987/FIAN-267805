b5a9eff637ad2925bc70904e189b47919400e9dd
Daniel Schirmer <d.schirmer1987@gmail.com>
2021-01-25 09:34:52 +0100

    Modulo Aufgaben

--- Klausurvorbereitung/src/modulo/EuroCent.java
+++ Klausurvorbereitung/src/modulo/EuroCent.java
@@ -1,10 +1,38 @@
 package modulo;
 
+import java.util.Scanner;
+
 public class EuroCent {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int eingabe, euro, cent;
+//		Scanner für das einlesen von der Konsole
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Bitte geben Sie einen Cent Betrag an:");
+		eingabe = sc.nextInt();
+//		Geteilt durch 100 für volle Euro
+		euro = eingabe / 100;
+		/*
+		 * Beispiel 256
+		 * 
+		 * 100 passen 2 mal in die 256
+		 * 
+		 * 100
+		 * 200 | 56
+		 * 
+		 * 256 / 100 = 2 Rest 56
+		 * 
+		 */
+//		Modulo rechnet die normale Division und gibt den Rest aus.
+		/*
+		 *  256 / 100 = 
+		 *  200 / 100 = 2 Rest 56
+		 *   56	
+		 */
+		cent = eingabe % 100;
+		
+		
+		System.out.println(eingabe + " Cent sind " + euro + " Euro und " + cent + " Cent");
 	}
 
 }
--- Klausurvorbereitung/src/modulo/SekundenZeit.java
+++ Klausurvorbereitung/src/modulo/SekundenZeit.java
@@ -1,10 +1,28 @@
 package modulo;
 
+import java.util.Scanner;
+
 public class SekundenZeit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int eingabe, sekunden, minuten, stunden, tage;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Bitte die Sekunden eingeben:");
+		eingabe = sc.nextInt();
+		
+//		1 Tag = 86400 Sekunden
+		tage = eingabe / 86400;
+//		1 Stunde = 3600 Sekunden bzw. Rest von Tage / 3600
+		stunden = (eingabe % 86400) / 3600;
+//		1 Minute = 60 Sekunden bzw. Rest von Stunden / 60
+		minuten = ((eingabe % 86400) % 3600) / 60;	
+//		1 Sekunde = 1 Sekunde
+		sekunden = eingabe % 60;
 
+
+
+		
+		System.out.println(eingabe +" Sekunden sind "+tage+" Tag(e) , "+stunden+" Stunde(n), "+minuten+" Minute(n) und "+sekunden+" Sekunde(n).");
 	}
 
 }
