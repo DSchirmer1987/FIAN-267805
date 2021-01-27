98fa77057eca44858e961eed551db26a939ca712
Daniel Schirmer <d.schirmer1987@gmail.com>
2021-01-27 12:45:34 +0100

    c

--- Klausurvorbereitung/src/oop_aufgabe2/konto/GiroKonto.java
+++ Klausurvorbereitung/src/oop_aufgabe2/konto/GiroKonto.java
@@ -1,5 +1,11 @@
 package oop_aufgabe2.konto;
 
+/**
+ * 
+ * @author Berliner
+ * extends = Erben / erweitern
+ * d.h. Unsere Klasse erbt von einer anderen
+ */
 public class GiroKonto extends Konto{
 
 	public GiroKonto(String vorname, String nachname, double kontostand) {
