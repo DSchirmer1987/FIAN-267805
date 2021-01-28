d1cc7a544b02729379301c93b610a48f68732ddd
Daniel Schirmer <d.schirmer1987@gmail.com>
2021-01-28 12:49:20 +0100

    Aufgabe Pizza

--- Klausurvorbereitung/src/arrays/ArrayAufgaben.java
+++ Klausurvorbereitung/src/arrays/ArrayAufgaben.java
@@ -35,6 +35,61 @@ public class ArrayAufgaben {
 		}
 		System.out.println("Mit von Zufallszahlen befüllt");
 		System.out.println(Arrays.toString(intArray));
+		
+		// Berechnen der Summe aller Zahlen im Array
+		int sum = 0;
+		for (int i = 0; i < intArray.length; i++) {
+			sum += intArray[i];
+		}
+		System.out.println("Summe aller Zahlen");
+		System.out.println(sum);
+		
+		// Berechnen des Mittelwertes aller Zahlen im Array
+		double average = 0;
+		average = (double) sum / (double) intArray.length;
+		System.out.println("Mittelwert aller Zahlen");
+		System.out.println(average);
+		
+		// Kleinste Zahl im Array
+		int min = intArray[0];
+		for (int i = 0; i < intArray.length; i++) {
+			if(min > intArray[i]) {
+				min = intArray[i];
+			}
+		}
+		System.out.println("Kleinste Zahl");
+		System.out.println(min);
+		
+		// Größte Zahl im Array
+		int max = intArray[0];
+		for (int i = 0; i < intArray.length; i++) {
+			if(max < intArray[i]) {
+				max = intArray[i];
+			}
+		}
+		System.out.println("Größte Zahl");
+		System.out.println(max);
+		
+		// Vertauschen / Umdrehen des Arrays
+		int[] intArray2 = new int[10];
+		counter = 0;
+		for (int i = 9; i >= 0; i--) {
+			intArray2[counter] = intArray[i];
+			counter++;
+		}
+		System.out.println(Arrays.toString(intArray));
+		System.out.println("Umgedrehtes Array");
+		System.out.println(Arrays.toString(intArray2));
+		
+		// Vertauschen / Umdrehen des Arrays ohne extra-Array
+		int tmp;
+		for (int i = 0; i < intArray.length / 2 ; i++) {
+			tmp = intArray[i];
+			intArray[i] = intArray[(intArray.length-1) - i];
+			intArray[(intArray.length-1) - i] = tmp;
+		}
+		System.out.println("Umgedrehtes Array ohne extra Array");
+		System.out.println(Arrays.toString(intArray2));
 	}
 
 }
