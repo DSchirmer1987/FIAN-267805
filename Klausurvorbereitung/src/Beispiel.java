ed3506900c4cea06c7f7f0cc56bb4ee6699d5272
Daniel Schirmer <d.schirmer1987@gmail.com>
2021-02-02 09:12:28 +0100

    c

--- Klausurvorbereitung/src/Beispiel.java
+++ Klausurvorbereitung/src/Beispiel.java
@@ -3,27 +3,19 @@ public class Beispiel {
 
 	public static void main(String[] args) {
 		int i = 6;
-		while (i <= 5) {
-			System.out.println("While:" + i);
-			i++;
+		System.out.println(i^=5);
+		System.out.println(i|=5);
+		System.out.println(i<<=5);
+		System.out.println(i>>=5);
+	}
+	
+	public static int unreachable(int i) {
+		if (i <= 5) {
+			return i;
+		} else if ( i == 5) {
+			return i;
 		}
-		
-		i = 6;
-		do {
-			System.out.println("Do-While:" +  i);
-			i++;
-		} while (i <= 5);
-		
-		
-		for(int j = 0; j <= 5; j++) {
-			System.out.println(j);
-		}
-		
-		
-		int[] array1 = new int[] {1,2,3,4,5};
-		for (int j : array1) {
-			System.out.println(j);
-		}
+		return i;
 	}
 
 }
